3cea5b55ccd5e7ec074eacb824b3b3e90f1588cb
kishan <kishankalavadiya@gmail.com>
2024-07-21 22:30:04 -0700

    add-item and display item router create done

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,11 +1,24 @@
-import Items from "../models/item.Model.js";
+import ItemModel from "../models/item.Model.js";
 
 import("colors");
 export const getItemController = async (req, res) => {
   try {
-    const items = await Items.find();
+    const items = await ItemModel.find();
     res.status(200).send(items);
   } catch (error) {
-    console.log(`Item Controller Error :- ${error}`.bgRed.inverse);
+    console.log(`Get-Item Controller Error :- ${error}`.bgRed.inverse);
+  }
+};
+
+export const addItemController = async (req, res) => {
+  try {
+    const newitem = new ItemModel(req.body);
+    // console.log(newitem);
+    await newitem.save();
+    res.status(201).json({ message: "Item Created Successfully!", newitem });
+    // res.status(201).send.json(newitem);
+  } catch (error) {
+    res.status(400).send("Error in Add Item", error);
+    console.log(`Add-Item Controller Error :- ${error}`.bgRed.inverse);
   }
 };
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getItemController } from "../controllers/itemController.js";
+import {
+  addItemController,
+  getItemController,
+} from "../controllers/itemController.js";
 
 const router = express.Router();
 
 //routes
 // Method - GET
 router.get("/get-item", getItemController);
+router.post("/add-item", addItemController);
 
 export default router;
