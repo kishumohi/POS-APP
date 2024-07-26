e72d081a30ff863149bd9bb07546a0bd11e7daed
kishan <kishankalavadiya@gmail.com>
2024-07-26 02:02:25 -0700

    done - category wise filter

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -11,6 +11,22 @@ import { HideLoading, ShowLoading } from "../redux/CartItemCount.js";
 function Homepage() {
   // Declare Variable
   const [itemsData, setItemsData] = useState([]);
+  // Filter by Category
+  const [selectedCategory, setSelectedCategory] = useState("drinks");
+  const categories = [
+    {
+      name: "drinks",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/430/430561.png",
+    },
+    {
+      name: "rice",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/3174/3174880.png",
+    },
+    {
+      name: "noodles",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/1471/1471262.png",
+    },
+  ];
   // console.log(itemsData);
   const dispatch = useDispatch();
   // Initial State Define and Data Get From API
@@ -31,12 +47,35 @@ function Homepage() {
 
   return (
     <DefaultLayout>
-      <Row>
-        {itemsData.map((item, index) => (
-          <Col key={index} xs={24} lg={6} md={12} sm={6}>
-            <ItemList item={item} />
-          </Col>
+      <div className="d-flex">
+        {categories.map((category) => (
+          <div
+            key={category.name}
+            className={`d-flex category ${
+              selectedCategory === category.name && "category-active"
+            }`}
+            onClick={() => {
+              setSelectedCategory(category.name);
+            }}
+          >
+            <h4>{category.name}</h4>
+            <img
+              src={category.imageUrl}
+              alt={category.imageUrl}
+              height={40}
+              width={60}
+            />
+          </div>
         ))}
+      </div>
+      <Row>
+        {itemsData
+          .filter((i) => i.category === selectedCategory)
+          .map((item, index) => (
+            <Col key={index} xs={24} lg={6} md={12} sm={6}>
+              <ItemList item={item} />
+            </Col>
+          ))}
       </Row>
     </DefaultLayout>
   );
